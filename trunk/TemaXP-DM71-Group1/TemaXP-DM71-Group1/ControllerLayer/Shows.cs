55f81647345466cd990d1b401492b559b73d6770
glerup9@hotmail.com <glerup9@hotmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-08 10:16:44 +0000

    lavet ControllerLayer mappe og begyndelsen på TestShows.cs og Shows.cs

---
+++ trunk/TemaXP-DM71-Group1/TemaXP-DM71-Group1/ControllerLayer/Shows.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemaXP_DM71_Group1.ControllerLayer
+{
+    class Shows
+    {
+        private void createShow(Movie movie, String time, DateTime date, Cinemas cinema)
+        {
+
+        }
+    }
+}
---
+++ trunk/TemaXP-DM71-Group1/TestCinemas/TestShows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCinemas
+{
+    [TestClass]
+    public class TestShows
+    {
+        Shows s = new Shows();
+        Movie m = new Movie();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String name = "test";
+            
+        }
+    }
+}
