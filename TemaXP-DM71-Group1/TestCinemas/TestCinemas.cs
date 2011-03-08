4f877a51ed15e3b81527e22ab903957fa59fcd76
daniel300689@gmail.com <daniel300689@gmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-08 09:06:11 +0000

    created TestCinemas

---
+++ TemaXP-DM71-Group1/TestCinemas/TestCinemas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCinemas
+{
+    [TestClass]
+    public class TestCinemas
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
