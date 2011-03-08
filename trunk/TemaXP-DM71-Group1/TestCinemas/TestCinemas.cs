4867876d130f600ffcdc91d9c6eb93a9e8c0e4e5
daniel300689@gmail.com <daniel300689@gmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-08 09:51:14 +0000

    finished Cinemas, and testet its functionality

--- trunk/TemaXP-DM71-Group1/TestCinemas/TestCinemas.cs
+++ trunk/TemaXP-DM71-Group1/TestCinemas/TestCinemas.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemaXP_DM71_Group1.ModelLayer;
 
 namespace TestCinemas
 {
@@ -12,6 +13,14 @@ namespace TestCinemas
         [TestMethod]
         public void TestMethod1()
         {
+            Cinemas c = new Cinemas();
+            Console.WriteLine("tester vi kan indsætte data");
+            String name = "gunner";
+            int seats = 45;
+            c.Name = name;
+            c.NrOfSeats = seats;
+            Assert.AreEqual(name, c.Name);
+            Assert.AreEqual(seats, c.NrOfSeats);
         }
     }
 }
