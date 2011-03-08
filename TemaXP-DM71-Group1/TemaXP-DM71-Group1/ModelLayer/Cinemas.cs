6afffa0301e6d925289066c99ee8d9080f19f623
daniel300689@gmail.com <daniel300689@gmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-08 09:51:14 +0000

    finished Cinemas, and testet its functionality

--- TemaXP-DM71-Group1/TemaXP-DM71-Group1/ModelLayer/Cinemas.cs
+++ TemaXP-DM71-Group1/TemaXP-DM71-Group1/ModelLayer/Cinemas.cs
@@ -5,12 +5,28 @@ using System.Text;
 
 namespace TemaXP_DM71_Group1.ModelLayer
 {
-    class Cinemas
+    public class Cinemas
     {
         private String name;
         private int nrOfSeats;
-        private IList<Seats> seats;
-        
-        
+        //private IList<Seats> seats;
+
+        public Cinemas()
+        {
+
+        }
+
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int NrOfSeats
+        {
+            get { return nrOfSeats; }
+            set { nrOfSeats = value; }
+        }
     }
 }
