7ca688757637b001259f53196401024bccec8db2
daniel300689@gmail.com <daniel300689@gmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-08 09:06:11 +0000

    created TestCinemas

---
+++ trunk/TemaXP-DM71-Group1/TemaXP-DM71-Group1/ModelLayer/Cinemas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemaXP_DM71_Group1.ModelLayer
+{
+    class Cinemas
+    {
+        private String name;
+        private int nrOfSeats;
+        private IList<Seats> seats;
+        
+        
+    }
+}
