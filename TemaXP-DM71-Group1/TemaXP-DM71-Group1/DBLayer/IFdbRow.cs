7d47698112d660fe806ecad7a4488f1ef02e8730
glerup9@hotmail.com <glerup9@hotmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-16 10:20:56 +0000

    IFdbRow tilføjet

---
+++ TemaXP-DM71-Group1/TemaXP-DM71-Group1/DBLayer/IFdbRow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TemaXP_DM71_Group1.ModelLayer;
+
+namespace TemaXP_DM71_Group1.DBLayer
+{
+    public interface IFdbRow
+    {
+        void InsertRow(Row row);
+
+        void DeleteRow(Row row);
+
+        void UpdateRow(Row row);
+
+        Row FindRowByRowNoAndCinema(int rowNo, Cinema cinema, bool retrieveAssociation);
+
+        Row FindRowById(Row row, bool retrieveAssociation);
+
+        IList<Row> FindAllRows(bool retrieveAssociation);
+    }
+}
