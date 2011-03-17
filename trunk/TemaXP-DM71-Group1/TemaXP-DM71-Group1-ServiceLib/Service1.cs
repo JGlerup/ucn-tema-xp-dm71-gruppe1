4fc721fd8994fe3d08fecb73f16d80ec00b0369d
erik.gravesen <erik.gravesen@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-17 11:20:32 +0000

    WCF implementeret

---
+++ trunk/TemaXP-DM71-Group1/TemaXP-DM71-Group1-ServiceLib/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TemaXP_DM71_Group1_ServiceLib.ControllerLayer;
+using TemaXP_DM71_Group1_ServiceLib.ModelLayer;
+
+namespace TemaXP_DM71_Group1_ServiceLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1
+    {
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+        //public IList<Show> FindAllShows(bool retrieveAssociation)
+        //{
+        //    CtrShow ctrShow = new CtrShow();
+        //    return ctrShow.FindAllShows(false);
+        //}
+    }
+}
