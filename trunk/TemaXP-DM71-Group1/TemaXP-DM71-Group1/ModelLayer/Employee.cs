048b0a15248a1c327c07f17617ebf242d60b1a01
anitalykkeclemmensen@gmail.com <anitalykkeclemmensen@gmail.com@d274dfb5-026e-4a07-5a3c-ddad42af6568>
2011-03-22 09:11:23 +0000

    3 stk nye klasser

---
+++ trunk/TemaXP-DM71-Group1/TemaXP-DM71-Group1/ModelLayer/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemaXP_DM71_Group1.ModelLayer
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ trunk/TemaXP-DM71-Group1/TemaXP-WCFServiceLib/ModelLayer/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemaXP_WCFServiceLib.ModelLayer
+{
+    class Booking
+    {
+        public int Id { get; set; }
+
+        public Show Show { get; set; }
+
+        public IList<Row> Rows { get; set; }
+
+        public IList<Seat> Seats { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
